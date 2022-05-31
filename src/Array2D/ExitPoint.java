242bab90e9e5d76f0f1b8e639430185d4bce4346
sameer <sk200112@gmail.com>
2022-05-31 15:17:23 +0530

    exit point added

---
+++ src/Array2D/ExitPoint.java
@@ -0,0 +1,52 @@
+package Array2D;
+import java.util.*;
+
+public class ExitPoint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int [][]arr = new int[n][m];
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int direction = 0;  // 0 : east , 1 : south , 2 : west , 3 : north
+        int row = 0;
+        int col = 0;
+        while(true){
+            direction = (direction + arr[row][col]) % 4;
+
+            if(direction == 0){        // east
+                col++;
+            }else if(direction == 1){  // south
+                row++;
+            }else if(direction == 2){  // west
+                col--;
+
+            }else if(direction == 3){ // north
+                row--;
+            }
+
+            if(row < 0){      // handling the case where we go out of array
+                row++;
+                break;
+            }else if(col < 0){
+                col++;
+                break;
+
+            }else if(row == arr.length){
+                row--;
+                break;
+            }else if(col == arr[0].length){
+                col--;
+                break;
+            }
+        }
+        System.out.println(row);
+        System.out.println(col);
+    }
+}
