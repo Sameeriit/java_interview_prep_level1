a5ae04cbdfb59418bb715e8c3f394a4ad896f4ec
sameer <sk200112@gmail.com>
2022-06-30 18:30:28 +0530

    more problems are added

--- src/Tree/GenircTree/GTreeTraversal.java
+++ src/Tree/GenircTree/GTreeTraversal.java
@@ -84,6 +84,14 @@ public class GTreeTraversal {
 
     public static void traversals(Node root){
         // write your code here
+        System.out.println("Node Pre"+" "+root.data);
+        for(Node child : root.children){
+            System.out.println("Edge Pre"+" "+root.data+"--"+child.data);
+            traversals(child);
+            System.out.println("Edge Post"+" "+root.data+"--"+child.data);
+
+        }
+        System.out.println("Node Post"+root.data);
 
 
     }
