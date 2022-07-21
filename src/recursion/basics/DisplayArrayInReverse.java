08cef51638cf4c8fe925fd00d20f2809c86a8185
sameer <sk200112@gmail.com>
2022-07-21 12:58:02 +0530

    more problems are added

--- src/recursion/basics/DisplayArrayInReverse.java
+++ src/recursion/basics/DisplayArrayInReverse.java
@@ -1,4 +1,4 @@
-package recursion;
+package recursion.basics;
 import java.util.*;
 
 /**
--- src/recursion/basics/basicRec.java
+++ src/recursion/basics/basicRec.java
@@ -1,4 +1,4 @@
-package recursion;
+package recursion.basics;
 
 public class basicRec {
     public static void main(String[] args) {
